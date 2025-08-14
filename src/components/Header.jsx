e6c314cc637454c3f36361ed43a7eb5b26984ad2
Hodman25 <ancaammaxamed1@gmail.com>
2025-08-14 11:37:19 +0300

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function Header() {
     return <>
-        <div className='flex justify-between px-20 py-3  '>
+        <div className='flex justify-between px-20 py-5 bg-white/5 backdrop-blur-sm fixed top-0 left-0 right-0 z-10'>
             <h1 className='text-4xl font-serif font-semibold '>Skill<span className='text-[#C30E59] '>Hub</span></h1>
             <ul className='flex gap-10  text-2xl '>
                 <Link to='/'><li>Home</li></Link>
