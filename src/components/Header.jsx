7071fd0fe39d2095b395dc7acac3bd55382ba690
jawhara2526 <zahraibraahim01@gmail.com>
2025-08-15 14:04:33 +0300

    e-learning

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,14 @@ import { Link } from 'react-router-dom';
 
 function Header() {
     return <>
-        <div className='flex justify-between px-20 py-5 bg-white/5 backdrop-blur-sm fixed top-0 left-0 right-0 z-10'>
+        <div className='flex justify-between px-20 py-5 bg-white/5 backdrop-blur-sm fixed top-0 left-0 right-0 z-10 text-white'>
             <h1 className='text-4xl font-serif font-semibold '>Skill<span className='text-[#C30E59] '>Hub</span></h1>
-            <ul className='flex gap-10  text-2xl '>
+            <ul className='flex gap-10  text-2xl  '>
                 <Link to='/'><li>Home</li></Link>
                 <Link to='/courses'><li>Courses</li></Link>
                 <Link to='/project'><li>Project</li></Link>
             </ul>
+            <button className='bg-[#C30E59] text-white text-2xl px-6 py-2 rounded-2xl mt-2'>Log In</button>
         </div>
     </>
 }
